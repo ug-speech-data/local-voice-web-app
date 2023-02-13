42697bee71f81b8d2526f991f3ffeaea4ac745a5
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2023-02-13 11:49:38 +0000

    fix: upload the enpoint to sdapi

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -9,42 +9,26 @@ function Dashboard() {
             <TopNav />
             <div className="p-2">
                 <div className="mx-auto d-flex flex-wrap col-lg-8">
-                    <h4>YOUR STATS</h4>
+                    <h4>DATA SUMMARY</h4>
                 </div>
                 <div className="mx-auto d-flex flex-wrap col-lg-8">
                     <DashboardCard >
                         <div className="d-flex justify-content-between mb-2">
-                            <h4 className="ms-0 my-0 p-0"><b>Images Submitted</b></h4>
+                            <h4 className="ms-0 my-0 p-0 h4"><b>Images</b></h4>
                             <h4 className="m-0 p-0"><b>10</b></h4>
                         </div>
                         <div className="d-flex justify-content-between mb-2">
-                            <p className="ms-3 my-1 p-0">Pending</p>
-                            <p className="m-0 p-0">10</p>
-                        </div>
-                        <div className="d-flex justify-content-between mb-2">
                             <p className="ms-3 my-1 p-0">Approved</p>
-                            <p className="m-0 p-0">10</p>
-                        </div>
-                        <div className="d-flex justify-content-between mb-2">
-                            <p className="ms-3 my-1 p-0">Rejected</p>
                             <p className="m-0 p-0">10</p>
                         </div>
                     </DashboardCard>
-                    <DashboardCard >
+                    <DashboardCard>
                         <div className="d-flex justify-content-between mb-2">
-                            <h4 className="ms-0 my-0 p-0"><b>Audios Submitted</b></h4>
+                            <h4 className="ms-0 my-0 p-0 h4"><b>Audios</b></h4>
                             <h4 className="m-0 p-0"><b>10</b></h4>
                         </div>
                         <div className="d-flex justify-content-between mb-2">
-                            <p className="ms-3 my-1 p-0">Pending</p>
-                            <p className="m-0 p-0">10</p>
-                        </div>
-                        <div className="d-flex justify-content-between mb-2">
                             <p className="ms-3 my-1 p-0">Approved</p>
-                            <p className="m-0 p-0">10</p>
-                        </div>
-                        <div className="d-flex justify-content-between mb-2">
-                            <p className="ms-3 my-1 p-0">Rejected</p>
                             <p className="m-0 p-0">10</p>
                         </div>
                     </DashboardCard>
