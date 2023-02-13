0eef4ff330c5f45383ac7a0482da0b51e63f4261
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2023-02-13 20:03:13 +0000

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_API_URI = process.env.NODE_ENV === 'production' ? 'https://sdapi.dodziraynard.me/api' : 'http://127.0.0.1:8000/api';
+export const BASE_API_URI = process.env.NODE_ENV === 'production' ? 'https://lvapi.dodziraynard.me/api' : 'http://127.0.0.1:8000/api';
