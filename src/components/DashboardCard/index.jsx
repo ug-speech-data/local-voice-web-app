0c7b78e3daf71c5c192b0bc1ddd489c0d9e3446e
dodizraynard <dodzireynard@gmail.com>
2023-03-04 10:17:07 +0000

    show dashboard statistics ;

--- src/components/DashboardCard/index.jsx
+++ src/components/DashboardCard/index.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 
 function DashboardCard(props) {
     return (
-        <div className="dashboard-card col-md-6 mt-4">
+        <div className="dashboard-card mt-4">
             <div className="card-content p-3">
                 {props.children}
             </div>
--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -1,26 +1,75 @@
 import TopNav from "../../components/TopNav";
-import { Link } from 'react-router-dom';
 import Footer from "../../components/Footer";
+import DashboardCard from "../../components/DashboardCard";
+import { BASE_API_URI } from '../../utils/constants';
+import { Fragment, useEffect, useState } from "react";
+import useAxios from '../../app/hooks/useAxios';
 
 
 function Dashboard() {
+    const { trigger: getDashboardStatics, data: responseData, error, isLoading } = useAxios({ mainUrl: `${BASE_API_URI}/dashboard-statistics/` });
+    const [statistics, setStatistics] = useState(null)
+
+    useEffect(() => {
+        getDashboardStatics()
+    }, [getDashboardStatics])
+
+    useEffect(() => {
+        if (Boolean(responseData?.statistics)) {
+            setStatistics(responseData.statistics)
+        }
+    }, [responseData, isLoading])
+
     return (
-        <>
+        <Fragment>
             <TopNav />
             <div className="p-2">
                 <div className="mx-auto d-flex flex-wrap col-lg-8">
                     <h4>DATA SUMMARY</h4>
                 </div>
-                <div className="mx-auto col-lg-8">
-                    <p className="text-warning">No info here</p>
+                {Boolean(statistics) ?
+                    <div className="mx-auto col-lg-8 row">
+                        <div className="col-md-6">
+                            <DashboardCard>
+                                <h5 className="h5">AUDIOS</h5>
+                                <div className="d-flex justify-content-between">
+                                    <p>Total Submitted: </p>
+                                    <p>{statistics?.audios_submitted} ({statistics?.audios_hours_submitted} hours)</p>
+                                </div>
+                                <hr />
+                                <div className="d-flex justify-content-between">
+                                    <p>Total Approved: </p>
+                                    <p>{statistics?.audios_approved} ({statistics?.audios_hours_approved} hours)</p>
+                                </div>
+                                <hr />
+                                <div className="d-flex justify-content-between">
+                                    <p>Total Transcribed: </p>
+                                    <p>{statistics?.audios_transcribed} ({statistics?.audios_hours_transcribed} hours)</p>
+                                </div>
+                                <hr />
+                            </DashboardCard>
+                        </div>
 
-                    <p className="my-4">
-                        Go to  <Link to="/profile"><button className="btn btn-sm btn-primary">Profile</button></Link>
-                    </p>
-                </div>
+                        <div className="col-md-6">
+                            <DashboardCard>
+                                <h5 className="h5">IMAGES</h5>
+                                <div className="d-flex justify-content-between">
+                                    <p>Total Submitted: </p>
+                                    <p>{statistics?.images_submitted}</p>
+                                </div>
+                                <hr />
+                                <div className="d-flex justify-content-between">
+                                    <p>Total Approved: </p>
+                                    <p>{statistics?.images_approved}</p>
+                                </div>
+                                <hr />
+                            </DashboardCard>
+                        </div>
+                    </div>
+                    : ""}
             </div>
             <Footer />
-        </>
+        </Fragment>
     );
 }
 
