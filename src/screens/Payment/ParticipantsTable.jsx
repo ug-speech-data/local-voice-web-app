ae86f41ed214f19a88392a2a6db46d4e12be741a
dodizraynard <dodzireynard@gmail.com>
2023-05-10 01:42:52 +0000

    pay paraticipants multiple times

--- src/screens/Payment/ParticipantsTable.jsx
+++ src/screens/Payment/ParticipantsTable.jsx
@@ -277,7 +277,7 @@ function ParticipantsTable() {
             </div>
 
             <div ref={editParticipantModalRef} className="modal fade" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-md">
+                <div className="modal-dialog modal-lg">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title">
@@ -324,18 +324,37 @@ function ParticipantsTable() {
                                     <label htmlFor="name" className="form-label"><b>Date</b></label>
                                     <p>{selectedParticipant?.created_at}</p>
                                 </div>
-
                                 <div className="my-3">
-                                    <label htmlFor="name" className="form-label"><b>Transaction ID</b></label>
-                                    <p>{selectedParticipant?.transaction?.transaction_id}</p>
-                                </div>
+                                        <table className="table">
+                                            <thead>
+                                                <tr>
+                                                    <th>Transaction ID</th>
+                                                    <th>Phone</th>
+                                                    <th>Amount</th>
+                                                    <th>Status</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {console.log(selectedParticipant?.transactions)}
+                                                {selectedParticipant?.transactions?.map(transaction =>
+                                                    <tr>
+                                                        <td>{transaction.transaction_id}</td>
+                                                        <td>{transaction.phone_number}</td>
+                                                        <td>{transaction.amount}</td>
+                                                        <td>
+                                                            {transaction?.status === "pending" ?
+                                                                <span className='badge bg-warning'>{transaction?.status}</span> :
+                                                                transaction?.status === "success" ?
+                                                                    <span className='badge bg-success'>{transaction?.status}</span> :
+                                                                    transaction?.status === "new" ?
+                                                                        <span className='badge bg-primary'>{transaction?.status}</span> :
+                                                                        <span className='badge bg-danger'>{transaction?.status}</span>}
 
-                                <div className="my-3">
-                                    <label htmlFor="name" className="form-label"><b>Status</b></label>
-                                    <p>
-                                        {selectedParticipant?.transaction === null && <span className="badge bg-warning">No transaction</span>}
-                                        <span className='badge bg-primary'>{selectedParticipant?.transaction?.status}</span>
-                                    </p>
+                                                        </td>
+                                                    </tr>
+                                                )}
+                                            </tbody>
+                                        </table>
                                 </div>
 
                                 <div className="my-3 d-flex justify-content-end">
@@ -364,7 +383,7 @@ function ParticipantsTable() {
                         { key: "type:INDEPENDENT", value: "Independent Participants" },
                     ]}
                     filters2={[
-                        { key: "transaction:null", value: "Not paid", defaultValue: true },
+                        { key: "balance__gt:0", value: "Not paid", defaultValue: true },
                         { key: "transaction__status:pending", value: "Pending transactions" },
                         { key: "transaction__status:failed", value: "Failed transactions" },
                         { key: "transaction__status:success", value: "Successful transactions" },
@@ -411,21 +430,22 @@ function ParticipantsTable() {
                     {
                         key: "percentage_audios_accepted", value: "% ACC."
                     }, {
-                        key: "amount", value: "Amount (₵)"
+                        key: "amount", value: "Amt. (₵)"
+                    }, {
+                        key: "balance", value: "Bal."
                     }, {
                         key: "paid", value: "Payment", render: (item) => {
                             return (
-                                <span>
-                                    {item?.transaction === null && <span className="badge bg-warning">No payment</span>}
-                                    {item?.transaction?.status === "pending" ?
-                                        <span className='badge bg-warning'>{item?.transaction?.status}</span> :
-                                        item?.transaction?.status === "success" ?
-                                            <span className='badge bg-success'>{item?.transaction?.status}</span> :
-                                            item?.transaction?.status === "new" ?
-                                                <span className='badge bg-primary'>{item?.transaction?.status}</span> :
-                                                <span className='badge bg-danger'>{item?.transaction?.status}</span>
-                                    }
-                                </span>
+                                <Fragment>
+                                    <span>
+                                        {item?.balance > 0 ?
+                                            <span className='badge bg-warning'>Pending</span> :
+                                            <span className='badge bg-success'>No Balance</span>
+                                        }
+                                    </span>
+                                    <br />
+                                    {item?.transactions?.length > 0 ? <span className="badge bg-primary p-1 m-0">previous: {item?.transactions?.length}</span> : ""}
+                                </Fragment>
                             )
                         }
                     },
